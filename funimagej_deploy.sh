eae53692f2f70dfaf445e57f26c092307595ed1f
Kyle Harrington <kharrington@uidaho.edu>
2017-07-28 10:44:56 +0200

    Adding dependencies to update site manually

--- funimagej_deploy.sh
+++ funimagej_deploy.sh
@@ -7,6 +7,11 @@ then
    unzip fiji-nojre.zip
    mv target/fun.imagej*jar Fiji.app/jars/
    cp -r src/plugins/* Fiji.app/plugins/
+   # Handle dependency fetching manually
+   cp ~/.m2/repository/net/imagej/imagej-mesh/$(ls ~/.m2/repository/net/imagej/imagej-mesh/ | tail -1)/*.jar .
+   cp ~/.m2/repository/random-forests-clj/random-forests-clj/$(ls ~/.m2/repository/random-forests-clj/random-forests-clj/ | tail -1)/*.jar .
+   cp ~/.m2/repository/clj-random/clj-random/$(ls ~/.m2/repository/clj-random/clj-random/ | tail -1)/*.jar .
+   cp ~/.m2/repository/seesaw/seesaw/$(ls ~/.m2/repository/seesaw/seesaw/ | tail -1)/*.jar .
    cd Fiji.app
    curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
    chmod a+x upload-site-simple.sh
