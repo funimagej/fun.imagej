bace3f567230c527165200ce832417339036fe84
Kyle Harrington <kharrington@uidaho.edu>
2018-03-11 22:48:20 -0700

    imagej-ops inc

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                                                                org.scijava/scripting-renjin]]
                  [ome/bioformats_package "5.3.3"]
                  ;[net.imagej/imagej-ops "0.38.1-SNAPSHOT"]
-                 [net.imagej/imagej-ops "0.38.0"]
+                 [net.imagej/imagej-ops "0.39.0"]
                  [net.imagej/imagej-mesh "0.2.0"]
 
                  [org.ojalgo/ojalgo "43.0"]
