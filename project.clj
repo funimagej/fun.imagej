2c5223b3adb17bbf85ee1f7244bb7829dab4acdb
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-05-04 10:10:18 -0400

    Clean project.clj and update sciview

--- project.clj
+++ project.clj
@@ -2,15 +2,11 @@
   :description "Functional Image Processing with ImageJ/FIJI"
   :url "https://github.com/funimagej/fun.imagel"
   :license {:name "Apache v2.0"
-            :url "https://github.com/funimage/funimage/LICENSE"}
+            :url "https://github.com/funimagej/fun.imagej/LICENSE"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [seesaw "1.4.4"]
-                 ;[me.raynes/fs "1.4.6"]
-                 ;[org.clojure/data.zip "0.1.1"]
                  [clj-random "0.1.8"]
 
-
-                 ;[cc.artifice/clj-ml "0.8.5"]
                  [random-forests-clj "0.2.0"]
 
                  ; Java libs
@@ -34,7 +30,7 @@
 
                  [org.scijava/scijava-common "2.76.0"]
 
-                 [sc.iview/sciview "0aa72f7" :exclusions [com.github.jnr/jffi
+                 [sc.iview/sciview "77c2afe" :exclusions [com.github.jnr/jffi
                                                           com.github.jnr/jnr-x86asm
                                                           org.scijava/scripting-renjin]]]
   :resource-paths ["src/main/resource"]
@@ -43,11 +39,7 @@
                  ["imagej-releases" "https://maven.imagej.net/content/repositories/releases/"]
                  ["ome maven" "https://artifacts.openmicroscopy.org/artifactory/maven/"]
                  ["imagej-snapshots" "https://maven.imagej.net/content/repositories/snapshots/"]
-                 ["jitpack.io" "https://jitpack.io"]
-                 ;["mosaic.public" "http://mosaic.mpi-cbg.de/maven/"]
-                 #_["clojars2" {:url "https://clojars.org/repo/"
-                                :username :env/LEIN_USERNAME
-                                :password :env/LEIN_PASSWORD}]]
+                 ["jitpack.io" "https://jitpack.io"]]
   :deploy-repositories [["releases" {:url "https://maven.imagej.net/content/repositories/releases"
                                      ;; Select a GPG private key to use for
                                      ;; signing. (See "How to specify a user
@@ -64,10 +56,7 @@
                                       :password :env/CI_DEPLOY_PASSWORD
                                       :sign-releases false}]]
   ; Try to use lein parent when we can
-  :plugins [[lein-cloverage "1.0.9"]
-            [lein-exec "0.3.7"]]
-  ;  :plugins [[lein-parent "0.3.1"]]
+  :plugins [[lein-exec "0.3.7"]]
   :jvm-opts ["-Xmx32g" "-server" "-Dscenery.Renderer=OpenGLRenderer"
              ;"-javaagent:/Users/kyle/.m2/repository/net/imagej/ij1-patcher/0.12.3/ij1-patcher-0.12.3.jar=init"
              #_"-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=localhost:8000"])
-  ;:javac-options ["-target" "1.6" "-source" "1.6"]
