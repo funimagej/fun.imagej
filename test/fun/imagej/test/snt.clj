e3db869954fca820cd4164c21a02d31d0237d498
Kyle Harrington <ornl@kyleharrington.com>
2022-05-31 15:29:50 -0400

    Disable SNT tests

--- test/fun/imagej/test/snt.clj
+++ test/fun/imagej/test/snt.clj
@@ -1,8 +1,8 @@
-(ns fun.imagej.test.snt
+#_(ns fun.imagej.test.snt
   (:import [sc.fiji.snt.annotation AllenUtils])
   (:require [clojure.test :refer :all]))
 
-(deftest allen-compartment
+#_(deftest allen-compartment
   (not
    (nil?
     (AllenUtils/getCompartment "Cerebral cortex"))))
